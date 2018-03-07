c27a4424445bb6d8cf9a698da11cc36fbbcea357
dongft <dongft@qq.com>
2018-03-07 23:12:46 +0800

    初始化项目，加入01

---
+++ springboot_01_helloworld/src/test/java/com/dft/demo/web/HelloControllerTests.java
@@ -0,0 +1,26 @@
+package com.dft.demo.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
