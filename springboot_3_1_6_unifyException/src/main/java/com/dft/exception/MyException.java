478a2334686a37f4aace459f76578b0536251e3b
dongfuting <dongfuting@hdsc.com>
2018-03-16 21:30:43 +0800

    3_2_1未完成

---
+++ springboot_3_1_6_unifyException/src/main/java/com/dft/exception/MyException.java
@@ -0,0 +1,12 @@
+package com.dft.exception;
+
+/**
+ * @Class Name MyException
+ * @Author dongfuting
+ * @Create In 2018年03月16日 20:13
+ */
+public class MyException extends Exception {
+  public MyException(String message) {
+    super(message);
+  }
+}
