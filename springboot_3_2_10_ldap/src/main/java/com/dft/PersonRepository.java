5cbff0728ef7c165b037be9cfb6799071340ff00
dongft <dongft@qq.com>
2018-03-21 21:27:59 +0800

    调整描述

--- springboot_3_2_10_ldap/src/main/java/com/dft/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dft;
-
-import org.springframework.data.repository.CrudRepository;
-
-import javax.naming.Name;
-
-public interface PersonRepository extends CrudRepository<Person, Name> {
-}
--- springboot_3_2_4_springDataJpaMultiDataSources/src/main/java/com/dft/domain/s/MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dft.domain.s;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
